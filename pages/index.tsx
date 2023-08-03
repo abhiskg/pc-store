e9dacefc411a9d6b3b69ddc091de6509860eb5a9
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-08-03 16:52:52 +0600

    middleware added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Layout from "@/components/layout/layout";
 import HeroSection from "@/components/sections/home/hero";
+import { useSession } from "next-auth/react";
 import { ReactElement } from "react";
 import { NextPageWithLayout } from "./_app";
 
 const HomePage: NextPageWithLayout = () => {
+  const { data } = useSession();
+  console.log(data);
   return (
     <div>
       <HeroSection />
