3c9d5fc5f33f8362f3da5f976955debc783257b3
Abhishek Sen Gupta <abhiskg20@gmail.com>
2023-08-04 16:29:36 +0600

    Category data fetched successfully

--- pages/pc-builder.tsx
+++ pages/pc-builder.tsx
@@ -1,8 +1,13 @@
+import { ICategory } from "@/backend/interfaces/categoryType";
 import Layout from "@/components/layout/layout";
+import axios from "axios";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { ReactElement } from "react";
-import { NextPageWithLayout } from "./_app";
 
-const PcBuilderPage: NextPageWithLayout = () => {
+const PcBuilderPage = ({
+  categories,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(categories);
   return <div>PcBuilderPage</div>;
 };
 
@@ -10,4 +15,16 @@ export default PcBuilderPage;
 
 PcBuilderPage.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>;
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  categories: ICategory[];
+}> = async () => {
+  const { data } = await axios.get(`${process.env.URL}/api/categories`);
+
+  return {
+    props: {
+      categories: data.data,
+    },
+  };
 };
